6d7635122f0bbe6754c0a35d7f161bdbe8744791
Oleksandra Tseselska <alexiyast@gmail.com>
2021-08-05 16:56:10 +0300

    home wokr 2

---
+++ js/custom.js
@@ -0,0 +1,14 @@
+var shipNumber = 4, cargoNumber = 6;
+confirm(shipNumber);
+confirm(cargoNumber);
+alert(shipNumber < cargoNumber);
+alert(shipNumber > cargoNumber);
+
+
+
+var routeEverestKilometers = alert(50);
+var routeGoverlaPounds = 56.4;
+var poundsVersusKilometers = 0.305;
+var routeGoverlaKilometers = alert(routeGoverlaPounds * poundsVersusKilometers);
+alert(routeEverestKilometers < routeGoverlaKilometers);
+alert(routeGoverlaKilometers > routeEverestKilometers);
