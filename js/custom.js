baa93c5a29defcb01bf97eb8ad940f3e25ea68f0
Oleksandra Tseselska <alexiyast@gmail.com>
2021-08-06 21:11:41 +0300

    added first part of homework 3

--- js/custom.js
+++ js/custom.js
@@ -3,3 +3,22 @@ var heightCylinder = 100;
 var volumeCylinder = Math.PI * Math.pow(radiusCylinder, 2) * heightCylinder;
 
 console.log('volumeCylinder --->', volumeCylinder);
+
+// Homework lesson 3
+var userAge =prompt('Сколько лет?');
+console.log('userAge-->', userAge);
+
+var userAge = +userAge;
+
+var userSmoke =confirm('Куришь?');
+console.log('userSmoke-->', userSmoke);
+
+if(userAge < 18 && userSmoke === false) {
+  alert('Так держать!')
+} else if(userAge < 18 && userSmoke === true) {
+  alert('Маме раскажу')
+} else if(userAge >= 18 && userSmoke === false) {
+  alert('Молодец, и не надо')
+}else if(userAge >= 18 && userSmoke === true) {
+  alert('Ну и зря')
+};
