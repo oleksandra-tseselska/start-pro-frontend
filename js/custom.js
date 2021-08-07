6654cb65095b3d785bd649f3555281ccc40c3a3f
Oleksandra Tseselska <alexiyast@gmail.com>
2021-08-07 10:31:05 +0300

    new first part of home work 2

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,14 @@
-var shipNumber = 4, cargoNumber = 6;
-confirm(shipNumber);
-confirm(cargoNumber);
-alert(shipNumber < cargoNumber);
-alert(shipNumber > cargoNumber);
+// First part home work 2
+
+var aNumber = +prompt('Set number a');
+var bNumber = +prompt('Set number b');
+console.log('aNumber-->', aNumber);
+console.log('bNumber-->', bNumber);
+if(aNumber > bNumber) {
+  alert('number a bigger than number b')
+} else if(bNumber > aNumber) {
+  alert('number b bigger than number a')
+};
 
 
 
