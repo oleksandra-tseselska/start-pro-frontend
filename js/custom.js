bd6a123d87363b1a542d3e758d11f397e1a1c5a0
Oleksandra Tseselska <alexiyast@gmail.com>
2021-08-12 11:44:05 +0300

    create second part

--- js/custom.js
+++ js/custom.js
@@ -22,3 +22,21 @@ if(userAge < 18 && userSmoke === false) {
 }else if(userAge >= 18 && userSmoke === true) {
   alert('Ну и зря')
 };
+
+
+// Homework lesson 3 part 2
+
+var armorColor =prompt('What color is the soldiers armor?');
+var haveShield =prompt('Does the soldier have a shield if there is one that is drawn?');
+
+function isLannisterSoldier(armorColor, haveShield){
+
+  if(armorColor === 'red' && haveShield === null || haveShield === 'lion') {
+    return true;
+  } else {
+    return false;
+  }
+    
+};
+
+console.log('isLannisterSoldier-->', isLannisterSoldier(armorColor, haveShield) );
